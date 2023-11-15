124f4cea6d150e145db825b2a04c62444928ca6a
Krzysztof J. Czarnecki <kjczarne@gmail.com>
2023-11-15 12:06:44 -0500

    Fixed tests, converted to `unittest`

--- test/test_read.py
+++ test/test_read.py
@@ -1,14 +1,16 @@
-import pytest
+import unittest
 from pathlib import Path
 from mdframe.reader import run, Config
 
 
-def test_metadata_print():
-    data_dir_path = Path(__file__).parent / "data_dir"
-    config = Config(data_dir_path, "toml", ["txt"])
-    df = run(config)
-    print(df)
+class TestReader(unittest.TestCase):
+
+    def test_metadata_print(self):
+        data_dir_path = Path(__file__).parent / "data_dir"
+        config = Config(data_dir_path, "toml")
+        df = run(config)
+        print(df)
 
 
 if __name__ == "__main__":
-    pytest.main()
+    unittest.main()
--- test/test_timing.py
+++ test/test_timing.py
@@ -1,19 +1,21 @@
-import pytest
+import unittest
 import pandas as pd
 from mdframe.timing import calc_time_and_rate_from_a_generic_df
 
 
-def test_calc_time_and_rate_from_a_generic_df():
-    df = pd.DataFrame({
-        "time": [
-            {"start": "2022-01-01 00:00:00", "end": "2022-01-01 00:00:10"},
-            {"start": "2022-01-01 00:00:10", "end": "2022-01-01 00:00:20"},
-            {"start": "2022-01-01 00:00:20", "end": "2022-01-01 00:00:30"}
-        ]
-    })
-    total_time, rate = calc_time_and_rate_from_a_generic_df(df)
-    assert total_time == 30.0
-    assert rate == 1.0
+class TestTiming(unittest.TestCase):
+
+    def test_calc_time_and_rate_from_a_generic_df(self):
+        df = pd.DataFrame({
+            "time": [
+                {"start": "2022-01-01 00:00:00", "end": "2022-01-01 00:00:10"},
+                {"start": "2022-01-01 00:00:10", "end": "2022-01-01 00:00:20"},
+                {"start": "2022-01-01 00:00:20", "end": "2022-01-01 00:00:30"}
+            ]
+        })
+        total_time, rate = calc_time_and_rate_from_a_generic_df(df)
+        self.assertEqual(total_time.total_seconds(), 30.0)
+        self.assertAlmostEqual(rate, 0.1)
 
 if __name__ == "__main__":
-    pytest.main()
+    unittest.main()
