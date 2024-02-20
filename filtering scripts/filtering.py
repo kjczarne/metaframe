5080b2fbe8f272035fac51d082e072bdc464cdea
nhatquang04 <nhatquangbui04@gmail.com>
2024-02-20 16:11:05 -0500

    Add script to filter and and or conditions

--- filtering scripts/filtering.py
+++ filtering scripts/filtering.py
@@ -22,7 +22,47 @@ def check_existence(arg: str, row: str) -> bool:
         
     return False
 
-def filter_files(data_dir_path: str, file_type: str,  **kwargs) -> list[str]:
+def filter_files_and(df_list: str,  **kwargs) -> list[str]:
+
+    statisfied_files = []
+
+    for df in df_list:
+        check_if_satisfied = True
+        for key, arg in kwargs.items():
+            if key not in list(df.index):
+                print(key + " is not a valid parameter")
+                continue
+            
+            row = list(df.loc[key,:].values)
+            if not check_existence(arg, row):
+                check_if_satisfied = False
+        
+        if check_if_satisfied:
+            statisfied_files.append(df.loc["nutrition_subgroup", "gid"])
+    
+    return sorted(statisfied_files)
+
+def filter_files_or(df_list: str,  **kwargs) -> list[str]:
+    statisfied_files = []
+
+    for df in df_list:
+        check_if_satisfied = False
+        for key, arg in kwargs.items():
+            if key not in list(df.index):
+                print(key + " is not a valid parameter")
+                continue
+            
+            row = list(df.loc[key,:].values)
+            if check_existence(arg, row):
+                check_if_satisfied = True
+        
+        if check_if_satisfied:
+            statisfied_files.append(df.loc["nutrition_subgroup", "gid"])
+    
+    return sorted(statisfied_files)
+
+
+def filter_files(data_dir_path: str, file_type: str, condition_and: bool,  **kwargs) -> list[str]:
     
     """Filters the dataframes based on the given parameters
     
@@ -37,42 +77,10 @@ def filter_files(data_dir_path: str, file_type: str,  **kwargs) -> list[str]:
     config = Config(data_dir_path, file_type)
     df_list = run(config)
 
-    satisfied_files = []
-    for df in df_list:
-        check_if_satisfied = False
-        for key, arg in kwargs.items():
-            if key not in list(df.index):
-                print(key + " is not a valid parameter")
-                continue
-
-            row = list(df.loc[key,:].values)
-
-            # if the arg is a list, check if all the items in the list exist in the row
-            if type(arg) == list:
-                condition_or = False
-                for item in arg:
-                    condition_and = True
-                    if type(item) == list:
-                        # if the item is a list, check if all the sub_items in the list exist in the row
-                        for sub_item in item:
-                            if not check_existence(sub_item, row):
-                                condition_and = False
-                    
-                    if condition_and and type(item) == list:
-                        condition_or = True
-
-                    elif check_existence(item, row):
-                        condition_or = True
-                if condition_or:
-                    check_if_satisfied = True
-
-            # if the arg is not a list, check if the arg exists in the row
-            elif check_existence(arg, row):
-                check_if_satisfied = True
-
-        # To prevent adding the same file multiple times
-        if check_if_satisfied:
-            satisfied_files.append(df.loc["nutrition_subgroup", "gid"])
+    if condition_and:
+        satisfied_files = filter_files_and(df_list, **kwargs)
+    else:
+        satisfied_files = filter_files_or(df_list, **kwargs)
 
     return sorted(satisfied_files)
 
@@ -81,14 +89,8 @@ if __name__ == "__main__":
     data_dir_path = Path(__file__).parent.parent / "sample toml files"
     file_type = "toml"
 
-    print(filter_files(data_dir_path, file_type, started="15:02:33") == [52])
-    print(filter_files(data_dir_path, file_type, texture_sources="IMG_7088") == [52])
-    # 1 bracket is condition or, 2 brackets is condition and
-    print(filter_files(data_dir_path, file_type, texture_sources=["IMG_7082"]) == [51])
-    print(filter_files(data_dir_path, file_type, texture_sources=["IMG_7088"]) == [52])
-    print(filter_files(data_dir_path, file_type, texture_sources=[["IMG_7088"], ["IMG_7082"]]) == [51, 52])
-
-    # This is condition and
-    print(filter_files(data_dir_path, file_type, weight = 160, quality_comments = "great individual scans, unable to merge")==[48])
+    print(filter_files(data_dir_path, file_type, condition_and=True, started="15:02:33") == [52])
+    print(filter_files(data_dir_path, file_type, condition_and=True, texture_sources="IMG_7088") == [52])
+    print(filter_files(data_dir_path, file_type, condition_and=False, weight = 160, quality_comments = "great individual scans, unable to merge")==[48])
     
     
