17ce9fedb75b6d694034c6a5905bbd0650f2515b
Krish Shah <shahkrish2016@gmail.com>
2024-03-06 19:04:09 -0500

    implemented flattening for validation

--- src/mdframe/reader.py
+++ src/mdframe/reader.py
@@ -11,6 +11,16 @@ SUPPORTED_DATA_FILE_EXTENSIONS = get_args(DataFileExtension)
 MetadataFileExtension = Literal["toml"]
 SUPPORTED_METADATA_FILE_EXTENSIONS = get_args(MetadataFileExtension)
 
+# TODO: add typing.Dict for argument, return
+def flatten_property_dict(property_dict: Dict) -> Dict:
+    flattened_dict = dict()
+    for property_name, value in property_dict.items():
+        if isinstance(value, dict):
+            for subproperty, subvalue in value.items():
+                flattened_dict[subproperty] = subvalue
+        else:
+            flattened_dict[property_name] = value
+    return flattened_dict
 
 def load_metadata_file(metadata_file_path: Path) -> Dict[str, Any]:
     if not metadata_file_path.exists():
@@ -19,8 +29,14 @@ def load_metadata_file(metadata_file_path: Path) -> Dict[str, Any]:
     with open(metadata_file_path, "r") as f:
         metadata_file_contents = toml.load(f)
 
-    return metadata_file_contents
+    # load key_properties
+    # make flat list out of metadata_file_content
 
+    # check if malformed here, add appropriate error messages here
+    # some property that should be filled in is missing in template file
+    # what are important properties: need to see a toml file to see all properties
+
+    return metadata_file_contents
 
 def metadata_file_to_df(metadata_file_contents):
     return pd.DataFrame(metadata_file_contents)
@@ -33,15 +49,15 @@ def data_dir_to_dataframes(data_dir_path: Path,
     metadata_records = map(metadata_file_to_df, metadata_file_contents)
     return list(metadata_records)
 
-
 @dataclass
 class Config:
     data_dir_path: Path
     metadata_file_extension: Optional[MetadataFileExtension]
     # data_file_extensions: List[DataFileExtension]
-
-
+    properties: List[str]
+    
 def run(config: Config):
+    print(**config.__dict__)
     return data_dir_to_dataframes(**config.__dict__)
 
 
--- test/test_read.py
+++ test/test_read.py
@@ -1,6 +1,6 @@
 import unittest
 from pathlib import Path
-from mdframe.reader import run, Config
+from mdframe.reader import run, Config, flatten_property_dict
 
 
 class TestReader(unittest.TestCase):
@@ -10,6 +10,24 @@ class TestReader(unittest.TestCase):
         config = Config(data_dir_path, "toml")
         df = run(config)
         print(df)
+
+    def test_flatten_property_dict(self):
+        input = {'gid': 50, 
+          'uid': 'f1e75821-e41f-4159-9050-def0d981b648', 
+          'item': 
+            {'nutrition_subgroup': 'ice cream and frozen dairy desserts', 'food_type': 'ice cream', 'description': 'Kawartha Death by Chocolate ice cream made with fresh milk and cream'}, 
+          'metrics': 
+            {'weight': 101, 'unit': 'g', 'ingredients': []}, 
+          'model': 
+            {'project_name': 'Project11192023134625', 'rgbd_file_names': ['50_icecream_1'],      'nutrition_facts_sources': ['IMG_7055', 'IMG_7056', 'IMG_7057'], 'texture_sources': ['IMG_7064', 'IMG_7065', 'IMG_7066', 'IMG_7067'], 'quality': 3, 'quality_comments': 'some minor holes near plate were filled, otherwise great detail'}, 
+          'time': 
+            {'started': '13:43:56', 'finished': '13:57:25'}}
+        desired_output = {'gid': 50, 
+          'uid': 'f1e75821-e41f-4159-9050-def0d981b648', 
+          'nutrition_subgroup': 'ice cream and frozen dairy desserts', 
+          'food_type': 'ice cream', 'description': 'Kawartha Death by Chocolate ice cream made with fresh milk and cream', 'weight': 101, 'unit': 'g', 'ingredients': [], 
+          'project_name': 'Project11192023134625', 'rgbd_file_names': ['50_icecream_1'],      'nutrition_facts_sources': ['IMG_7055', 'IMG_7056', 'IMG_7057'], 'texture_sources': ['IMG_7064', 'IMG_7065', 'IMG_7066', 'IMG_7067'], 'quality': 3, 'quality_comments': 'some minor holes near plate were filled, otherwise great detail', 'started': '13:43:56', 'finished': '13:57:25'}
+        assert desired_output == flatten_property_dict(input)
 
 
 if __name__ == "__main__":
