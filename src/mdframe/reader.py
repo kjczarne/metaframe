5a61fee842aeed3024f62027a29f27cf693496b5
Krish Shah <shahkrish2016@gmail.com>
2024-03-06 19:40:42 -0500

    implemented schema validation

--- src/mdframe/reader.py
+++ src/mdframe/reader.py
@@ -4,6 +4,8 @@ import toml
 from typing import Dict, Any, Tuple, Optional, Literal, get_args, List
 from pathlib import Path
 from dataclasses import dataclass
+from jsonschema import validate
+from schema import validation_schema
 
 DataFileExtension = Literal["txt", "jpg"]
 SUPPORTED_DATA_FILE_EXTENSIONS = get_args(DataFileExtension)
@@ -11,30 +13,14 @@ SUPPORTED_DATA_FILE_EXTENSIONS = get_args(DataFileExtension)
 MetadataFileExtension = Literal["toml"]
 SUPPORTED_METADATA_FILE_EXTENSIONS = get_args(MetadataFileExtension)
 
-# TODO: add typing.Dict for argument, return
-def flatten_property_dict(property_dict: Dict) -> Dict:
-    flattened_dict = dict()
-    for property_name, value in property_dict.items():
-        if isinstance(value, dict):
-            for subproperty, subvalue in value.items():
-                flattened_dict[subproperty] = subvalue
-        else:
-            flattened_dict[property_name] = value
-    return flattened_dict
-
-def load_metadata_file(metadata_file_path: Path) -> Dict[str, Any]:
+def load_metadata_file(metadata_file_path: Path, schema: Dict) -> Dict[str, Any]:
     if not metadata_file_path.exists():
         raise ValueError(f"Path {metadata_file_path} does not seem to point to a valid file")
 
     with open(metadata_file_path, "r") as f:
         metadata_file_contents = toml.load(f)
 
-    # load key_properties
-    # make flat list out of metadata_file_content
-
-    # check if malformed here, add appropriate error messages here
-    # some property that should be filled in is missing in template file
-    # what are important properties: need to see a toml file to see all properties
+    validate(instance = metadata_file_contents, schema = schema)
 
     return metadata_file_contents
 
@@ -43,9 +29,10 @@ def metadata_file_to_df(metadata_file_contents):
 
 
 def data_dir_to_dataframes(data_dir_path: Path,
-                           metadata_file_extension: MetadataFileExtension) -> List[pd.DataFrame]:
-    metadata_file_paths = data_dir_path.glob(f"*.{metadata_file_extension}")
-    metadata_file_contents = map(load_metadata_file, metadata_file_paths)
+                           metadata_file_extension: MetadataFileExtension,
+                           schema: Dict) -> List[pd.DataFrame]:
+    metadata_file_paths = data_dir_path.glob(f"*.{metadata_file_extension}")    
+    metadata_file_contents = [load_metadata_file(path, schema) for path in metadata_file_paths]
     metadata_records = map(metadata_file_to_df, metadata_file_contents)
     return list(metadata_records)
 
@@ -54,7 +41,7 @@ class Config:
     data_dir_path: Path
     metadata_file_extension: Optional[MetadataFileExtension]
     # data_file_extensions: List[DataFileExtension]
-    properties: List[str]
+    schema: Dict
     
 def run(config: Config):
     print(**config.__dict__)
@@ -73,8 +60,9 @@ def main():
                         default="toml")
     args = parser.parse_args()
     config = Config(
-        Path(args.directory),
-        args.metadata_ext,
+        data_dir_path = Path(args.directory),
+        metadata_file_extension = args.metadata_ext,
+        schema = validation_schema
     )
     dfs = run(config)
     print(dfs)
---
+++ src/mdframe/schema.py
@@ -0,0 +1,135 @@
+schema = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "gid": {
+      "type": "integer"
+    },
+    "uid": {
+      "type": "string"
+    },
+    "item": {
+      "type": "object",
+      "properties": {
+        "nutrition_subgroup": {
+          "type": "string"
+        },
+        "food_type": {
+          "type": "string"
+        },
+        "description": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "nutrition_subgroup",
+        "food_type",
+        "description"
+      ]
+    },
+    "metrics": {
+      "type": "object",
+      "properties": {
+        "weight": {
+          "type": "integer"
+        },
+        "unit": {
+          "type": "string"
+        },
+        "ingredients": {
+          "type": "array",
+          "items": {}
+        }
+      },
+      "required": [
+        "weight",
+        "unit",
+        "ingredients"
+      ]
+    },
+    "model": {
+      "type": "object",
+      "properties": {
+        "project_name": {
+          "type": "string"
+        },
+        "rgbd_file_names": {
+          "type": "array",
+          "items": [
+            {
+              "type": "string"
+            }
+          ]
+        },
+        "nutrition_facts_sources": {
+          "type": "array",
+          "items": [
+            {
+              "type": "string"
+            },
+            {
+              "type": "string"
+            },
+            {
+              "type": "string"
+            }
+          ]
+        },
+        "texture_sources": {
+          "type": "array",
+          "items": [
+            {
+              "type": "string"
+            },
+            {
+              "type": "string"
+            },
+            {
+              "type": "string"
+            },
+            {
+              "type": "string"
+            }
+          ]
+        },
+        "quality": {
+          "type": "integer"
+        },
+        "quality_comments": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "project_name",
+        "rgbd_file_names",
+        "nutrition_facts_sources",
+        "texture_sources",
+        "quality",
+        "quality_comments"
+      ]
+    },
+    "time": {
+      "type": "object",
+      "properties": {
+        "started": {
+          "type": "string"
+        },
+        "finished": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "started",
+        "finished"
+      ]
+    }
+  },
+  "required": [
+    "gid",
+    "uid",
+    "item",
+    "metrics",
+    "model",
+    "time"
+  ]
+}
